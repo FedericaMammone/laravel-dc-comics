c251adeabf647268580979e441498eb9822e4220
Fef97 <federicakr97@gmail.com>
2023-07-17 16:28:28 +0200

    stampati in pagina i dati

--- resources/views/comic/index.blade.php
+++ resources/views/comic/index.blade.php
@@ -1,4 +1,27 @@
 @extends('layouts.main-layout')
 
 @section('content')
+    <div class="text-center">
+        {{-- <h1>
+        Pastas
+        <a href="{{ route('pastas.create') }}">+</a>
+    </h1> --}}
+        <h2>Main Comics</h2>
+        <ul class="list-unstyled p-5">
+            @foreach ($comics as $comic)
+                <li class="m-5 bg-secondary">
+                    {{-- <a href="{{ route('pastas.show', $pasta->id) }}">
+                    {{ $pasta->titolo }}
+                </a> --}}
+                    <img width="250px" src=" {{ $comic->thumb }}" alt="">
+                    {{ $comic->title }} <br> <br>
+                    {{ $comic->description }} <br> <br>
+                    {{ $comic->price }} <br> <br>
+                    {{ $comic->series }} <br> <br>
+                    {{ $comic->sale_date }} <br> <br>
+                    {{ $comic->type }}
+                </li>
+            @endforeach
+        </ul>
+    </div>
 @endsection
